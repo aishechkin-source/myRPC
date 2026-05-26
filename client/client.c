c217815ec1fc5cba38f9b48f6fce9ad22976feba
Ishechkin Alex <aishechkin@gmail.com>
2026-05-26 14:59:33 +0300

    клиент: добавлен HTTP-клиент для отправки запросов

--- client/client.c
+++ client/client.c
@@ -4,87 +4,59 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
+#define PORT 8080
+#define BUF_SIZE 4096
 
 int main(int argc, char *argv[]) {
-    char *host = "127.0.0.1";
-    int port = 9090;
-    char command[256] = {0};
-
-    int opt;
-
-    // getopt
-    while ((opt = getopt(argc, argv, "h:p:c:")) != -1) {
-        switch (opt) {
-            case 'h':
-                host = optarg;
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case 'c':
-                strncpy(command, optarg, sizeof(command) - 1);
-                break;
-            default:
-                printf("Использование: %s -h <host> -p <port> -c \"command\"\n", argv[0]);
-                exit(1);
-        }
+    if (argc != 4) {
+        printf("Usage: %s <IP> <meta 0|1> <token>\n", argv[0]);
+        return 1;
     }
 
-    if (strlen(command) == 0) {
-        printf("Команда не указана!\n");
-        exit(1);
-    }
+    char *ip = argv[1];
+    char *meta = argv[2];
+    char *token = argv[3];
 
     int sock;
     struct sockaddr_in server_addr;
+    char request[1024];
+    char buffer[BUF_SIZE];
 
+    /* socket */
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror("socket");
-        exit(1);
+        perror("Socket error");
+        return 1;
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
-    if (inet_pton(AF_INET, host, &server_addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        exit(1);
+    /* connect */
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        return 1;
     }
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("connect");
-        exit(1);
-    }
-
-    printf("Подключено к серверу\n");
-
-    // JSON запрос
-    char request[BUFFER_SIZE];
+    /* HTTP request */
     snprintf(request, sizeof(request),
-             "{\"login\":\"alex\",\"command\":\"%s\"}", command);
+        "GET /index.html?meta=%s HTTP/1.1\r\n"
+        "Host: %s:%d\r\n"
+        "Authorization: %s\r\n\r\n",
+        meta, ip, PORT, token);
 
+    /* send */
     send(sock, request, strlen(request), 0);
 
-    // ЧТЕНИЕ ОТВЕТА (🔥 исправлено)
-    char response[BUFFER_SIZE];
-    int total = 0;
+    /* receive */
     int bytes;
-
-    while ((bytes = read(sock, response + total, BUFFER_SIZE - total - 1)) > 0) {
-        total += bytes;
+    while ((bytes = recv(sock, buffer, sizeof(buffer)-1, 0)) > 0) {
+        buffer[bytes] = '\0';
+        printf("%s", buffer);
     }
-
-    if (bytes < 0) {
-        perror("read");
-    }
-
-    response[total] = '\0';
-
-    printf("Ответ: %s\n", response);
 
     close(sock);
-
     return 0;
 }
+
